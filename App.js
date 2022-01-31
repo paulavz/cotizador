fa8858e43e52fa8d9164987e9ce96c5dcdaa49c0
Paula Suarez <psuarez@AURUS.CL>
2022-01-31 12:10:57 -0300

    Video 11

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, SafeAreaView, View, StatusBar, Button } from "react-native";
+import Form from "./src/components/Form.js";
+import colors from "./src/utils/colors.js";
 
 export default function App() {
+  const [capital, setCapital] = useState(null);
+  const [interest, setInterest] = useState(null);
+  const [months, setMonths] = useState(null);
+
+  const onSubmit = () => {
+    console.log("capital =>", capital);
+    console.log("interest =>", interest);   
+    console.log("months =>", months);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.background} />
+        <Text style={styles.titleApp}>Cotizador de Prestamos</Text>
+        <Form
+          setCapital={setCapital}
+          setInterest={setInterest}
+          setMonths={setMonths}
+        />
+      </SafeAreaView>
+      <View>
+        <Text>BODY</Text>
+      </View>
+      <View>
+        <Button title="Enviar" onPress={onSubmit}/>
+        <Text>FOOOTER</Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  safeArea: {
+    height: 290,
+    alignItems: "center",
+  },
+  background: {
+    position: "absolute",
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 200,
+    width: "100%",
+    zIndex: -1,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  titleApp: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 15,
   },
 });
---
+++ src/components/Form.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import colors from "../utils/colors";
+import RNPickerSelect from "react-native-picker-select";
+
+export default function Form(props) {
+  const { setCapital, setInterest, setMonths } = props;
+  return (
+    <View style={styles.viewForm}>
+      <View style={styles.viewInputs}>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Cantidad a pedir"
+          onChange={e=>setCapital(e.nativeEvent.text)}
+        />
+        <TextInput
+          style={[styles.input, styles.inputPercentaje]}
+          placeholder="Interes %"
+          keyboardType="numeric"
+          onChange={e=>setInterest(e.nativeEvent.text)}
+        />
+      </View>
+      <RNPickerSelect
+        style={picketSelectStyles}
+        onValueChange={(value) => setMonths(value)}
+        items={[
+          { label: "3 meses", value: 3 },
+          { label: "6 meses", value: 6 },
+          { label: "12 meses", value: 12 },
+          { label: "24 meses", value: 24 },
+        ]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewForm: {
+    position: "absolute",
+    bottom: 0,
+    width: "85%",
+    paddingHorizontal: 58,
+    backgroundColor: colors.PRIMARY_COLOR_DARK,
+    borderRadius: 30,
+    height: 180,
+    justifyContent: "center",
+  },
+  viewInputs: {
+    flexDirection: "row",
+  },
+  input: {
+    height: 50,
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: colors.PRIMARY_COLOR,
+    borderRadius: 5,
+    width: "60%",
+    marginRight: 5,
+    marginLeft: -5,
+    marginBottom: 10,
+    color: "#000",
+    paddingHorizontal: 20,
+  },
+  inputPercentaje: {
+    width: "40%",
+    marginLeft: 5,
+  },
+});
+
+const picketSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 4,
+    color: "black",
+    paddingRight: 30,
+    backgroundColor: "#fff",
+    marginLeft: -5,
+    marginRight: -5,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 15,
+    color: "black",
+    paddingRight: 30,
+    backgroundColor: "#fff",
+    marginLeft: -5,
+    marginRight: -5,
+  },
+});
